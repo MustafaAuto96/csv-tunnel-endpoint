5d334627500464e7947675ef138ed2e464c1e94f
MustafaAuto96 <flashvg88@gmail.com>
2025-11-04 03:09:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/csv-tunnel-endpoint/',
   plugins: [react()],
   build: {
     outDir: 'build',
